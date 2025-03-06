f160fc49d6a39629edfca0442079f1198629d80c
kalema1 <kalemar8@gmail.com>
2025-03-06 16:28:45 +0300

    styles: add styles to the Users component
    - added styles to make table width scrollable on the mobile screens.

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -241,35 +241,37 @@ export default function Users() {
           <h2 className={`header-secondary ${styles.userheading}`}>
             List Of Users
           </h2>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                <th className={styles.tableHeader}>Name</th>
-                <th className={styles.tableHeader}>Email</th>
-                <th className={styles.tableHeader}>Phone</th>
-                <th className={styles.tableHeader}>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id} className={styles.tableRow}>
-                  <td className={styles.tableData}>{user.name}</td>
-                  <td className={styles.tableData}>{user.email}</td>
-                  <td className={styles.tableData}>{user.phone}</td>
-                  <td className={styles.tableData}>
-                    <div className={styles.btnWrapper}>
-                      <button className={styles.btn}>
-                        <IoIosCreate className={styles.editIcon} />
-                      </button>
-                      <button className={styles.btn}>
-                        <IoMdTrash className={styles.deleteIcon} />
-                      </button>
-                    </div>
-                  </td>
+          <div className={styles.tableWrapper}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th className={styles.tableHeader}>Name</th>
+                  <th className={styles.tableHeader}>Email</th>
+                  <th className={styles.tableHeader}>Phone</th>
+                  <th className={styles.tableHeader}>Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id} className={styles.tableRow}>
+                    <td className={styles.tableData}>{user.name}</td>
+                    <td className={styles.tableData}>{user.email}</td>
+                    <td className={styles.tableData}>{user.phone}</td>
+                    <td className={styles.tableData}>
+                      <div className={styles.btnWrapper}>
+                        <button className={styles.btn}>
+                          <IoIosCreate className={styles.editIcon} />
+                        </button>
+                        <button className={styles.btn}>
+                          <IoMdTrash className={styles.deleteIcon} />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </section>
