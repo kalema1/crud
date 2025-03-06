a446395ac3ce522b8d3e9c642ede2c3ea460963a
kalema1 <kalemar8@gmail.com>
2025-03-06 16:06:02 +0300

    feat: add user component
    - created the Users component.
    - added jsx to the component.
    - added styles to the component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import AddUser from "./components/addUser/AddUser";
 import SearchUser from "./components/searchUser/SearchUser";
+import Users from "./components/users/Users";
 
 function App() {
   return (
     <>
       <SearchUser />
       <AddUser />
+      <Users />
     </>
   );
 }
